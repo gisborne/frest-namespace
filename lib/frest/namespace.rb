f8fd06a71636caaca2e906f5084f0a068c0ae544
Guyren Howe <guyren@relevantlogic.com>
2016-06-17 01:39:37 -0700

    Support deleting an entire predicate.

--- lib/frest/namespace.rb
+++ lib/frest/namespace.rb
@@ -13,61 +13,52 @@ module Frest
     DEFAULT_STORE_ID  = 'default'
 
     def get_connection(
-        file:
+        file: DEFAULT_DB
     )
       f               = File.absolute_path(file) #canonicalize connection by full path
       @connections[f] ||= SQLite3::Database.new(f)
-      @connections[f].create_function 'uuid', 0 do |func, value|
+      result = @connections[f]
+      result.create_function 'uuid', 0 do |func, value|
         func.result = SecureRandom.uuid
       end
+
+      result
     end
 
     def set(
-        store_id: DEFAULT_STORE_ID,
         id:,
-        db: DEFAULT_DB,
         values:,
-        insert_values: method(:insert_values),
-        delete_values: method(:delete_values),
-        **context)
+        store_id: DEFAULT_STORE_ID,
+        db: DEFAULT_DB,
+        insertfn: method(:insert_values),
+        deletefn: method(:delete),
+        **c)
 
       c = get_connection file: db
 
-      insert_hash, delete_hash = values.partition{|k, v| v}.map &:to_h
-      insert_values(connection: c, id: id, insert_hash: insert_hash, store_id: store_id, **context)
-      delete_values(connection: c, id: id, keys: delete_hash.keys, store_id: store_id, **context)
+      insert_hash, delete_hash = values.partition { |_, v| v }.map &:to_h
+      insertfn.call(connection: get_connection, id: id, insert_hash: insert_hash, store_id: store_id, **c)
+      deletefn.call(connection: get_connection, id: id, keys: delete_hash.keys, store_id: store_id, **c)
     end
 
-    def insert_values connection:, id:, insert_hash:, store_id:, **context
-      return if insert_hash.count == 0
-      insert_hash.each { |k, v|
-        insert_hash[k] = prepare_value(value: v, **context)
-      }
-
-      keys_string   = "(id, #{insert_hash.keys * ','})"
-      values_string = insert_hash.map do |k, v|
-        "('#{id}', '#{k}', #{v})"
-      end * ",\n"
-
+    def delete(
+        connection:,
+        id:,
+        store_id:,
+        keys: nil,
+        **c)
+      return if keys != nil && keys.count == 0
       #TODO respect subtables
-      sql = %{
-        INSERT OR REPLACE INTO #{store_id}_simple(id, key, value)
-        VALUES#{values_string}
-      }
 
-      connection.execute sql
-    end
-
-
-    def delete_values connection:, id:, keys:, store_id:, **context
-      return if keys.count == 0
-      #TODO respect subtables
       sql = %{
         DELETE FROM #{store_id}_simple
         WHERE
-          id = '#{id}' AND
-          key IN (#{keys.map{|k| "'#{k}'"} * ','})
+          id = '#{id}'
       }
+      sql += %{
+        AND
+          key IN (#{keys.map { |k| "'#{k}'" } * ','})
+      } if keys
 
       connection.execute sql
     end
@@ -78,7 +69,7 @@ module Frest
         id:,
         db: DEFAULT_DB,
         subtables: DEFAULT_SUBTABLES,
-        **context)
+        **c)
 
       c      = get_connection file: db
 
@@ -96,31 +87,104 @@ module Frest
     end
 
 
-
     def setup(
         id: 'default',
         db: DEFAULT_DB,
         subtables: DEFAULT_SUBTABLES,
-        **context)
+        **c)
 
       c = get_connection file: db
 
       subtables.each do |subtbl|
+        sql = %{
+          CREATE TABLE IF NOT EXISTS #{id}_#{subtbl}_src(
+            id UUID NOT NULL,
+            key text NOT NULL,
+            value text,
+            created date DEFAULT CURRENT_TIMESTAMP,
+            PRIMARY KEY(id, key)
+          )
+        }
+
+        c.execute sql
+
         c.execute %{
-            CREATE TABLE #{id}_#{subtbl}(
-              id UUID NOT NULL,
-              key text NOT NULL,
-              value text,
-              created date DEFAULT CURRENT_TIMESTAMP,
-              PRIMARY KEY(id, key)
-            )
-          }
+          CREATE TABLE IF NOT EXISTS #{id}_#{subtbl}_history_src(
+            id UUID NOT NULL,
+            key text NOT NULL,
+            value text,
+            created date DEFAULT CURRENT_TIMESTAMP,
+            PRIMARY KEY(id, key, created)
+          )
+        }
+
+        c.execute "
+          CREATE VIEW IF NOT EXISTS
+            #{id}_#{subtbl} AS
+          SELECT
+            *
+          FROM
+            #{id}_#{subtbl}_src"
+
+        sql = %{
+          CREATE TRIGGER IF NOT EXISTS
+            #{id}_#{subtbl}_uuid_trigger
+          INSTEAD OF
+            INSERT
+          ON
+            #{id}_#{subtbl}
+          FOR EACH ROW
+          BEGIN
+            INSERT INTO
+              #{id}_#{subtbl}_src(
+                id,
+                key,
+                value)
+              SELECT
+                COALESCE(NEW.id, UUID()),
+                NEW.key,
+                NEW.value;
+          END
+        }
+
+        # p sql
+        c.execute sql
       end
     end
 
 
-    def prepare_value value:, **context
+    def prepare_value(
+        value:,
+        **c)
       "'#{SQLite3::Database.quote(value.to_s)}'"
+    end
+
+
+    private
+
+    def insert_values(
+        connection: get_connection,
+        id:,
+        insert_hash:,
+        store_id:,
+        **c)
+      return if insert_hash.count == 0
+      insert_hash.each { |k, v|
+        insert_hash[k] = prepare_value(value: v, **c)
+      }
+
+      keys_string   = "(id, #{insert_hash.keys * ','})"
+      values_string = insert_hash.map do |k, v|
+        "('#{id}', '#{k}', #{v})"
+      end * ",\n"
+
+      #TODO respect subtables
+      sql           = %{
+           INSERT OR REPLACE INTO #{store_id}_simple(id, key, value)
+           VALUES#{values_string}
+      }
+
+      connection.execute sql
     end
   end
 end
